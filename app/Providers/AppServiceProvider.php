0a966585cf0c25da21f54c67b2362f316fdd7a2a
RhinoCan <29071660+RhinoCan@users.noreply.github.com>
2026-05-22 15:43:31 -0400

    Only make custom Faker providers visible in development/test environments

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,21 +22,20 @@ class AppServiceProvider extends ServiceProvider
    */
   public function boot(): void
   {
-    // functions techSkills and techSkillsCsv displays 1 to 4 Laravel/PHP skills
-    // function techTitles displays a random IT job title
-    $this->app->make(Generator::class)
-      ->addProvider(
-        new DevGigsProvider(
-          $this->app->make(Generator::class)
-        )
-      );
+    if ($this->app->environment('local', 'testing')) {
+      $this->app->make(Generator::class)
+        ->addProvider(
+          new DevGigsProvider(
+            $this->app->make(Generator::class)
+          )
+        );
 
-    // Displays a Canadian city for use in Gigs portion of the app
-    $this->app->make(Generator::class)
-      ->addProvider(
-        new CanadaDataProvider(
-          $this->app->make(Generator::class)
-        )
-      );
+      $this->app->make(Generator::class)
+        ->addProvider(
+          new CanadaDataProvider(
+            $this->app->make(Generator::class)
+          )
+        );
+    }
   }
 }
